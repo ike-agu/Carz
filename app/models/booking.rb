0eae8d90841579422822221eb809d4077b43d434
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-09-29 22:58:19 +0100

    created some files in database

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :user
-  belongs_to :car
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  # belongs_to :user
+  # belongs_to :car
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
 end

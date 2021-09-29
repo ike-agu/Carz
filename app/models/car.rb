eec0cc5d80b90bd01fdc248458a915f9c6d3657f
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-09-30 00:10:58 +0100

    Generated some seed files

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,9 @@
 class Car < ApplicationRecord
-  # belongs_to :user
-  # has_many :bookings
-  # validates :name, presence: true, uniqueness: true
-  # validates :model, presence: true
-  # validates :price, presence: true
-  # validates :description, presence: true
-  # validates :rating, inclusion: { in: 1..5 }
+  belongs_to :user
+  has_many :bookings
+  validates :name, presence: true, uniqueness: true
+  validates :model, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :rating, inclusion: { in: 1..5 }
 end

99af7a67874c4ee2287fe34a972e3be4704613a5
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-10-01 00:07:36 +0100

    backend work done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,26 +8,35 @@ class CarsController < ApplicationController
     @cars = Car.all
   end
 
-  # def new
-  #  @car = Car.new
-  # end
+  def new
+    @car = Car.new
+  end
 
-  # def create
-  #  @car = car.new(car_params)
-  #  @car.user = current_user
-  # end
+  def create
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to car_path(@car)
+  end
 
-  # def edit
-  #  @car = Car.find_by_id(params[:id])
-  # end
+  def edit
+    @car = Car.find(params[:id])
+  end
 
-  # def update
-  #  @car = Car.find_by_id(params[:id])
-  #  @car = Car.update(island_params)
-  # end
+  def update
+    @car = Car.find_by_id(params[:id])
+    @car = Car.update(car_params)
+    redirect_to car_path(@car)
+  end
 
-  # def destroy
-  #  @car = Cars.find(params[:id])
-  #  @car.destroy
-  # end
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :model, :price, :rating, photos:[])
+  end
 end

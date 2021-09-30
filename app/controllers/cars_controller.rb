abdf2d4986644cc6b4faa306b8c30959d5fe4aa1
cornelius cole <corneliuscole@hotmail.com>
2021-09-30 10:13:12 +0100

    controller update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,25 +10,25 @@ class CarsController < ApplicationController
   # end
 
   # def new
-  #   @car = Car.new
+  #  @car = Car.new
   # end
 
   # def create
-  #   @car = car.new(car_params)
-  #   @car.user = current_user
+  #  @car = car.new(car_params)
+  #  @car.user = current_user
   # end
 
   # def edit
-  #   @car = Car.find_by_id(params[:id])
+  #  @car = Car.find_by_id(params[:id])
   # end
 
   # def update
-  #   @car = Car.find_by_id(params[:id])
-  #   @car = Car.update(island_params)
+  #  @car = Car.find_by_id(params[:id])
+  #  @car = Car.update(island_params)
   # end
 
   # def destroy
-  #   @car = Cars.find(params[:id])
-  #   @car.destroy
+  #  @car = Cars.find(params[:id])
+  #  @car.destroy
   # end
 end

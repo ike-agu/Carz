ce540c0497d22fb86a1f6b7b02966e46bd98c555
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-10-01 14:25:55 +0100

    Added link to create a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    @booking.save
+    # flash[:notice] = @booking.errors.full_messages.to_sentence unless @booking.save
+     redirect_to car_path(@car)
   end
 
   private
 
   def find_car
     @car = Car.find(params[:car_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
